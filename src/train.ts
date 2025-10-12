e8491f53e5270c841a37d97c860378d0e4886493
yakuboff-m <m.yakuboff.15@gmail.com>
2025-10-13 07:10:18 +0900

    feat: TASK H

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,23 @@
-// < ========== TASK G start ========== >
+// < ========== TASK H Start ========== >
 
-function getHighestIndex(arr: number[]) {
-  const maxValue = Math.max(...arr);
-  const index = arr.indexOf(maxValue);
-  return index;
+function getPositive(arr: number[]): string {
+  return arr
+    .filter((num) => num > 0)
+    .map((num) => String(num))
+    .join("");
 }
 
-console.log(getHighestIndex([4, 65, 23, 43, 765]));
+console.log(getPositive([5, -7, 2]));
+// < ========== TASK H end ========== >
+
+// < ========== TASK G start ========== >
+
+// function getHighestIndex(arr: number[]) {
+//   const maxValue = Math.max(...arr);
+//   const index = arr.indexOf(maxValue);
+//   return index;
+// }
+
+// console.log(getHighestIndex([4, 65, 23, 43, 765]));
 
 // < ========== TASK G end ========== >
