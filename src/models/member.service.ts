f048c6dc8ddba05c2d51288d1f68bbb48575801d
yakuboff-m <m.yakuboff.15@gmail.com>
2025-10-19 08:12:57 +0900

    fix: modify restaurant login and singup process

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Error";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -16,6 +17,12 @@ class MemberService {
       .exec();
     // console.log("exist:", exist);
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    console.log("before:", input.memberPassword);
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    console.log("after:", input.memberPassword);
+
     try {
       const result = await this.memberModel.create(input);
       // const tempResult = new this.memberModel(input);
@@ -36,14 +43,17 @@ class MemberService {
       .exec();
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+    );
+    // const isMatch = input.memberPassword === member.memberPassword;
 
     if (!isMatch) {
-        throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
-    };
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
 
     return await this.memberModel.findById(member._id).exec();
-
   }
 }
 
