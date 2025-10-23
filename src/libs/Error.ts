a984816aaae6dc36ec0f18ac6ef2fb39eb0ed028
yakuboff-m <m.yakuboff.15@gmail.com>
2025-10-23 15:39:27 +0900

    feat: create checkAuthSession api

--- src/libs/Error.ts
+++ src/libs/Error.ts
@@ -18,6 +18,7 @@ export enum Message {
     USED_NICK_PHONE = "You are inserting already used nick or phone!",
     NO_MEMBER_NICK = "NO member with that member nick!",
     WRONG_PASSWORD = "Wrong password entered, please try again!",
+    NOT_AUTHENTICATED = "You are not authenticated, Please, login first!"
 }
 
 class Errors extends Error {
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -10,6 +10,8 @@ routerAdmin
 routerAdmin
   .get("/signup", restaurantController.getSignup)
   .post("/signup", restaurantController.processSignup);
+routerAdmin
+  .get("/check-me", restaurantController.checkAuthSession)
 
 /** PRODUCT **/ 
 
