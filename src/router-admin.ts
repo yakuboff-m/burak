fd762c2e37af45e2c3c80026d3c9a48accc8fa26
yakuboff-m <m.yakuboff.15@gmail.com>
2025-10-26 07:01:01 +0900

    feat: create product controller and product routing api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 /** RESTAURANT **/
 routerAdmin.get("/", restaurantController.goHome);
@@ -14,6 +15,9 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** PRODUCT **/
-
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin
+  .post("/product/creare", productController.createNewProduct)
+  .post("product/:id", productController.updateChosenProduct);
 /** USER **/
 export default routerAdmin;
