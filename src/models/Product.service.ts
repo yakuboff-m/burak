b3fada5e6541985c002834818de208f4f92be566
yakuboff-m <m.yakuboff.15@gmail.com>
2025-10-27 07:36:02 +0900

    feat: configure product Schema Model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,4 +1,11 @@
+import ProductModel from "../schema/Product.model";
 
-class ProductService {}
+class ProductService {
+    private readonly productModel;
+
+    constructor() {
+        this.productModel = ProductModel;
+    }
+}
 
 export default ProductService;
