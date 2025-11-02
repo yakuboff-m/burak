083c4ef6b0087936d50a9d663bdacb785558b36f
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-03 05:58:46 +0900

    feat: developing frontend signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,30 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      console.log("uploadFile:", uploadFile);
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please, insert only jpg, jpeg, png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -22,4 +46,23 @@ function validateSignupForm() {
     alert("Password differs, please confirm your password");
     return false;
   }
+
+  const memberImage = $(".member-image").get(0).files(0)
+    ? $(".member-image").get(0).files(0).name
+    : null;
+  if(!memberImage) {
+    alert("Please, insert restaurant image!");
+    return false;
+  }
 }
+
+
+// const inputMember = $(".member-image").get(0);
+//   const memberImage =
+//     inputMember && inputMember.files.length > 0
+//       ? $(".member-image").get(0).files[0].name
+//       : null;
+//   if (!memberImage) {
+//     alert("Please Instert Restaurant Image!");
+//     return false;
+//   }
