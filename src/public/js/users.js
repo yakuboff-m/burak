29cee355321820c4745802957f59b1fdc65e0357
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-05 08:33:16 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,26 @@
 console.log("Users frontend javascript file");
+
+$(".member-status").on("change", function (e) {
+  const id = e.target.id;
+  console.log("id:", id);
+
+  const memberStatus = $(`#${id}.member-status`).val();
+  console.log("memberStatus:", memberStatus);
+
+  axios
+    .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+    .then((response) => {
+        console.log("response:", response);
+        const result = response.data;
+        console.log("result:", response);
+
+        if(result.data) {
+            console.log("User updated!");
+            $(".member-status").blur();
+        } else alert("User update failed!");
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("User update failed");
+    });
+});
