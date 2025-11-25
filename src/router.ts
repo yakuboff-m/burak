b548113a78296a5168c4b380fe7ed95ae1ba0c81
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-26 08:41:32 +0900

    feat: develop getTopUsers Rest API

--- src/router.ts
+++ src/router.ts
@@ -22,6 +22,7 @@ router.post(
   uploader("members").single("memberImage"),
   memberController.updateMember
 );
+router.get("/member/top-users", memberController.getTopUsers); 
 
 /** Product **/
 
