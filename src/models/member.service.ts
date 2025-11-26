20f852a86cc71cfe33b177812141e71e662d9da8
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-26 14:34:11 +0900

    feat: develop getRestaurant rest api

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -21,6 +21,17 @@ class MemberService {
   }
 
   //SPA
+  public async getRestaurant(): Promise<Member> {
+    const result = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURANT })
+      .lean()
+      .exec();
+    if (!result) 
+      throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
   public async signup(input: MemberInput): Promise<Member> {
     console.log("before:", input.memberPassword);
     const salt = await bcrypt.genSalt();
