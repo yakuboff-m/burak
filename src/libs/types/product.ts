10c42a3c8c8dc9bd3dce4c56071996759fd2d8c4
yakuboff-m <m.yakuboff.15@gmail.com>
2025-12-07 07:51:34 +0900

    feat: CORS integration

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -15,7 +15,10 @@ export interface Product {
     productDesc?: string,
     productImages: string[],
     productViews: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
+
 
 export interface ProductInquiry {
     order: string,
